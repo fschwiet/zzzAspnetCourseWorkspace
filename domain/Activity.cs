8b9cfb3b6a2333e7d46676f5c7290f023687787c
Frank <fschwiet@gmail.com>
2021-04-01 14:07:03 -0300

    add a domain project

---
+++ domain/Activity.cs
@@ -0,0 +1,13 @@
+
+using System;
+
+namespace domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
