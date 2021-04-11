f8866c8ff448aec3a58402e9337ee32097e6d86c
Frank <fschwiet@gmail.com>
2021-04-11 20:48:16 -0300

    running first migration on startup

--- api/Program.cs
+++ api/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using persistence;
 
 namespace api
 {
@@ -13,7 +16,25 @@ namespace api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            
+            using var scope = host.Services.CreateScope();
+
+            try
+            {
+                var dataContext = scope.ServiceProvider.GetService<DataContext>();
+                dataContext.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                var logger = scope.ServiceProvider.GetService<Logger<Program>>();
+
+                logger.LogCritical("Error running migrations: " + e);
+
+                throw;
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
