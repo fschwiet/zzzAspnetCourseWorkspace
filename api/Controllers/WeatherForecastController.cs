69fdfaf9fe85f6b470db2c82f9c34b51ab7eb06b
Frank <fschwiet@gmail.com>
2021-04-11 21:47:00 -0300

    adding an Activities controller

--- api/Controllers/WeatherForecastController.cs
+++ api/Controllers/WeatherForecastController.cs
@@ -7,9 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace api.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : BaseApiController
     {
         private static readonly string[] Summaries = new[]
         {
