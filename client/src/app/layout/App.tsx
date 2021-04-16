38945db003c0c8f49a5c939cb23dc0ab7cdc87da
Frank <fschwiet@gmail.com>
2021-04-16 14:31:37 -0300

    add a navigation bar to the client

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
   let [activities, setActivities] = useState<Activity[]>([]);
@@ -14,15 +15,17 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <Header as="h2" icon="users" content="This is a header."></Header>
-      <List>
-        {
-          activities.map((activity) =>
-            <List.Item key={activity.id}>{activity.title}</List.Item>)
-        }
-      </List>
-    </div>
+    <>
+      <NavBar/>
+      <Container style={{marginTop: '7em'}}>
+        <List>
+          {
+            activities.map((activity) =>
+              <List.Item key={activity.id}>{activity.title}</List.Item>)
+          }
+        </List>
+      </Container>
+    </>
   );
 }
 
