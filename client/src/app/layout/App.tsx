085f8090c7283b75493bd5ec20288c64c05daf83
Frank <fschwiet@gmail.com>
2021-04-19 16:40:11 -0300

    adding delete button

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -38,6 +38,10 @@ function App() {
     setSelectedActivity(activity);
   }
 
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(x => x.id !== id)]);
+  }
+
   useEffect(() => {
     axios.get<Activity[]>("https://localhost:5001/api/Activities").then(response => {
       console.log(response);
@@ -56,7 +60,8 @@ function App() {
           editMode={editMode}
           openForm={handleFormOpen}
           closeForm={handleFormClose} 
-          createOrEdit={handleCreateOrEditActivity}/>
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}/>
       </Container>
     </>
   );
--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -3,11 +3,12 @@ import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 
 interface Props {
-  activities: Activity[],
-  selectActivity: (id: string) => void
+  activities: Activity[];
+  selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
 }
 
-export default function ActivityList({ activities, selectActivity }: Props) {
+export default function ActivityList({ activities, selectActivity, deleteActivity }: Props) {
   return (
     <Segment>
       <Item.Group divided>
@@ -18,12 +19,13 @@ export default function ActivityList({ activities, selectActivity }: Props) {
                 <Item.Header as="a">{activity.title}</Item.Header>
                 <Item.Meta>{activity.date}</Item.Meta>
                 <Item.Description>
-                <div>{activity.description}</div>
-                <div>{activity.city}, {activity.venue}</div>
+                  <div>{activity.description}</div>
+                  <div>{activity.city}, {activity.venue}</div>
                 </Item.Description>
                 <Item.Extra>
-                  <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue"/>
-                  <Label basic content={activity.category}/>
+                <Button onClick={() => selectActivity(activity.id)} floated="right" content="View" color="blue" />
+                <Button onClick={() => deleteActivity(activity.id)} floated="right" content="Delete" color="red" />
+                  <Label basic content={activity.category} />
                 </Item.Extra>
               </Item.Content>
             </Item>
