e891533a4b18a3757370e9ffc6450bc9ac8a6836
fschwiet <fschwiet@gmail.com>
2021-06-15 21:59:59 -0700

    added logged-in check to some react routes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -11,10 +11,10 @@ import TestErrors from '../../features/errors/TestErrors';
 import { ToastContainer } from 'react-toastify'
 import NotFound from '../../features/errors/NotFound';
 import ServerError from '../../features/errors/ServerError';
-import LoginForm from '../../features/users/LoginForm';
 import { useStore } from '../../stores/store';
 import LoadingComponent from './LoadingComponent';
 import ModalContainer from '../common/modals/ModalContainer';
+import PrivateRoute from './PrivateRoute';
 
 function App() {
   const location = useLocation();
@@ -40,12 +40,11 @@ function App() {
           <NavBar />
           <Container style={{ marginTop: '7em' }}>
             <Switch>
-              <Route exact path='/activities' component={ActivitiesDashboard} />
-              <Route path='/activities/:id' component={ActivityDetails} />
-              <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-              <Route path='/errors' component={TestErrors} />
+              <PrivateRoute exact path='/activities' component={ActivitiesDashboard} />
+              <PrivateRoute path='/activities/:id' component={ActivityDetails} />
+              <PrivateRoute key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <PrivateRoute path='/errors' component={TestErrors} />
               <Route path='/server-error' component={ServerError} />
-              <Route path='/login' component={LoginForm} />
               <Route component={NotFound} exact />
             </Switch>
           </Container>
